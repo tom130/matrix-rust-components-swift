4f5fe12854cab04726ab792f1fe02a637a74a6c1
Tomas Krbec <mail@tomakrbec.cz>
2026-05-29 02:23:12 +0200

    Point package at watchOS xcframework

--- Package.swift
+++ Package.swift
@@ -1,13 +1,14 @@
 // swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
-let checksum = "c8aba389d8b3cff31ef06451657a030e1b7f64d9e6fb883ae4f190c68ecf1123"
-let version = "26.05.21-2"
-let url = "https://github.com/element-hq/matrix-rust-components-swift/releases/download/\(version)/MatrixSDKFFI.xcframework.zip"
+let checksum = "3c128857b111d468fea2da5bfd82dd0e1aa50e0b5e243ce3549ee364b0b1be36"
+let version = "26.05.21-2+watchos.1"
+let url = "https://github.com/tom130/matrix-rust-components-swift/releases/download/\(version)/MatrixSDKFFI.xcframework.zip"
 let package = Package(
     name: "MatrixRustSDK",
     platforms: [
-        .iOS(.v16)
+        .iOS(.v16),
+        .watchOS(.v10)
     ],
     products: [
         .library(name: "MatrixRustSDK", type: .dynamic, targets: ["MatrixRustSDK"]),
