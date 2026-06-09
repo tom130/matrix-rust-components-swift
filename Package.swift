76a891324e99a805d155779938c0ea1fb8664383
Tomas Krbec <mail@tomakrbec.cz>
2026-06-09 17:38:58 +0200

    Use SwiftPM distinct watchOS DNS override version

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 let checksum = "12f7842e92fd5d8fa0a6a9007babf2b06b3ae279979fb29be11f42e6147eedb4"
-let version = "26.05.21-2+watchos.2"
+let version = "26.05.21-2-watchos.2"
 let url = "https://github.com/tom130/matrix-rust-components-swift/releases/download/\(version)/MatrixSDKFFI.xcframework.zip"
 let package = Package(
     name: "MatrixRustSDK",
