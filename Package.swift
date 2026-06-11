c88f80bd5ac689770e5b78b888168d6db93deff4
Tomas Krbec <mail@tomakrbec.cz>
2026-06-11 20:04:05 +0200

    Update watchOS artifact checksum

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
-let checksum = "4bf83124d6c78407fb7b37669d69d2a09e45f7f0b30f5d934e0ec31375b8f982"
+let checksum = "5aed3b62e2b2126ec6ca531b6f490c555c5da0525830d006e263cef4673216a2"
 let version = "26.05.21-2-watchos.3"
 let url = "https://github.com/tom130/matrix-rust-components-swift/releases/download/\(version)/MatrixSDKFFI.xcframework.zip"
 let package = Package(
