8c3842ba8f12a685c382754e71596855f0364900
Tomas Krbec <mail@tomakrbec.cz>
2026-06-16 14:36:57 +0200

    Publish QR pairing watchOS SDK artifact

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
-let checksum = "5aed3b62e2b2126ec6ca531b6f490c555c5da0525830d006e263cef4673216a2"
-let version = "26.05.21-2-watchos.3"
+let checksum = "6e70f5c0ae336cc8842d80538a1412b9b3312ba3f89317b570e1d32f79870792"
+let version = "26.05.21-2-watchos.4"
 let url = "https://github.com/tom130/matrix-rust-components-swift/releases/download/\(version)/MatrixSDKFFI.xcframework.zip"
 let package = Package(
     name: "MatrixRustSDK",
